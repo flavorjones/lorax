26b7a8d709e258d143e02a12a9539c07b6a23348
Mike Dalessio <mike@csa.net>
2009-12-04 22:46:21 -0500

    setting up test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,5 @@
+$VERBOSE = true
+
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
@@ -6,5 +8,12 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'diffaroo'
 
-class Test::Unit::TestCase
+warn "#{__FILE__}:#{__LINE__}: libxml version info: #{Nokogiri::VERSION_INFO.inspect}"
+
+module Diffaroo
+  class TestCase < Test::Unit::TestCase
+    unless RUBY_VERSION >= '1.9'
+      undef :default_test
+    end
+  end
 end
