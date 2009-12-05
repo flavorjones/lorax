832427a6bbcb4486cf108e0cf6679627d67e9309
Mike Dalessio <mike@csa.net>
2009-12-04 22:47:45 -0500

    specified and implemented xml node hashing

---
+++ lib/diffaroo.rb
@@ -0,0 +1,9 @@
+require 'nokogiri'
+
+module Diffaroo
+  VERSION = "0.1.0"
+  REQUIRED_NOKOGIRI_VERSION = "1.4.0"
+  raise LoadError, "diffaroo requires Nokogiri version #{REQUIRED_NOKOGIRI_VERSION} or higher" unless Nokogiri::VERSION >= REQUIRED_NOKOGIRI_VERSION
+end
+
+require 'diffaroo/hash'
---
+++ lib/diffaroo/hash.rb
@@ -0,0 +1,21 @@
+require 'digest/sha1'
+
+module Diffaroo
+  module Hash
+    SEP = "\0"
+
+    def Hash.node_hash(node)
+      raise ArgumentError, "node_hash expects a Node, but received #{node.inspect}" unless node.is_a?(Nokogiri::XML::Node)
+
+      if node.text?
+        Digest::SHA1.hexdigest(node.content)
+      elsif node.element?
+        children_hash = Digest::SHA1.hexdigest node.children       .collect { |child| node_hash(child) }.join(SEP)
+        attr_hash =     Digest::SHA1.hexdigest node.attributes.sort.collect { |k,v| [k, v.value] }.flatten.join(SEP)
+        Digest::SHA1.hexdigest [node.name, attr_hash, children_hash].join(SEP)
+      else
+        raise ArgumentError, "node_hash expects a text node or element, but received #{node.type}"
+      end
+    end
+  end
+end
