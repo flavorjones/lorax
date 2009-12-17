5b4fe339800a35a02569e724e03db8b11a70d1b5
Mike Dalessio <mike@csa.net>
2009-12-16 21:58:08 -0500

    restructured the hash spec

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -20,141 +20,139 @@ describe Diffaroo::Hash do
         attr = doc.at_css("a1").attributes.first.last
         proc { Diffaroo::Hash.node_hash(attr) }.should raise_error(ArgumentError) 
       end
+
+      it "hashes each node only once" do
+        doc = xml { root {
+            a1 {
+              b1 {
+                c1 "hello"
+              }
+            }
+          } }
+        node = doc.at_css "c1"
+        mock.proxy(Diffaroo::Hash).node_hash(anything).times(5)
+        Diffaroo::Hash.node_hash(doc.root)
+      end
     end
 
-    context "XML" do
-      context "identical text nodes" do
+    context "identical text nodes" do
+      it "hashes equally" do
+        doc = xml { root {
+            span "hello"
+            span "hello"
+          } }
+        assert_node_hash_equal(*doc.css("span").collect { |n| n.children.first })
+      end
+    end
+
+    context "different text nodes" do
+      it "hashes differently" do
+        doc = xml { root {
+            span "hello"
+            span "goodbye"
+          } }
+        assert_node_hash_not_equal(*doc.css("span").collect { |n| n.children.first })
+      end
+    end
+
+    context "elements with same name (with no attributes and no content)" do
+      it "hashes equally" do
+        doc = xml { root { a1 ; a1 } }
+        assert_node_hash_equal(*doc.css("a1"))
+      end
+    end
+
+    context "elements with different names" do
+      it "hashes differently" do
+        doc = xml { root { a1 ; a2 } }
+        assert_node_hash_not_equal doc.at_css("a1"), doc.at_css("a2")
+      end
+    end
+
+    context "same elements in different docs" do
+      it "hashes equally" do
+        doc1 = xml { root { a1 } }
+        doc2 = xml { root { a1 } }
+        assert_node_hash_equal doc1.at_css("a1"), doc2.at_css("a1")
+      end
+    end
+
+    context "elements with same name and content (with no attributes)" do
+      context "and content is the same" do
         it "hashes equally" do
           doc = xml { root {
-              span "hello"
-              span "hello"
-            } }
-          assert_node_hash_equal(*doc.css("span").collect { |n| n.children.first })
-        end
-      end
-
-      context "different text nodes" do
-        it "hashes differently" do
-          doc = xml { root {
-              span "hello"
-              span "goodbye"
-            } }
-          assert_node_hash_not_equal(*doc.css("span").collect { |n| n.children.first })
-        end
-      end
-
-      context "elements with same name (with no attributes and no content)" do
-        it "hashes equally" do
-          doc = xml { root { a1 ; a1 } }
-          assert_node_hash_equal(*doc.css("a1"))
-        end
-      end
-
-      context "elements with different names" do
-        it "hashes differently" do
-          doc = xml { root { a1 ; a2 } }
-          assert_node_hash_not_equal doc.at_css("a1"), doc.at_css("a2")
-        end
-      end
-
-      context "same elements in different docs" do
-        it "hashes equally" do
-          doc1 = xml { root { a1 } }
-          doc2 = xml { root { a1 } }
-          assert_node_hash_equal doc1.at_css("a1"), doc2.at_css("a1")
-        end
-      end
-
-      context "elements with same name and content (with no attributes)" do
-        context "and content is the same" do
-          it "hashes equally" do
-            doc = xml { root {
-                a1 "hello"
-                a1 "hello"
-              } }
-            assert_node_hash_equal(*doc.css("a1"))
-          end
-        end
-
-        context "and content is not the same" do
-          it "hashes equally" do
-            doc = xml { root {
-                a1 "hello"
-                a1 "goodbye"
-              } }
-            assert_node_hash_not_equal(*doc.css("a1"))
-          end
-        end
-      end
-
-      context "elements with same name and children (with no attributes)" do
-        context "and children are in the same order" do
-          it "hashes equally" do
-            doc = xml { root {
-                a1 { b1 ; b2 }
-                a1 { b1 ; b2 }
-              } }
-            assert_node_hash_equal(*doc.css("a1"))
-          end
-        end
-
-        context "and children are not in the same order" do
-          it "hashes differently" do
-            doc = xml { root {
-                a1 { b1 ; b2 }
-                a1 { b2 ; b1 }
-              } }
-            assert_node_hash_not_equal(*doc.css("a1"))
-          end
-        end
-      end
-
-      context "elements with same name and same attributes (with no content)" do
-        it "hashes equally" do
-          doc = xml { root {
-              a1("foo" => "bar", "bazz" => "quux")
-              a1("foo" => "bar", "bazz" => "quux")
+              a1 "hello"
+              a1 "hello"
             } }
           assert_node_hash_equal(*doc.css("a1"))
         end
       end
 
-      context "elements with same name and different attributes (with no content)" do
-        it "hashes differently" do
+      context "and content is not the same" do
+        it "hashes equally" do
           doc = xml { root {
-              a1("foo" => "bar", "bazz" => "quux")
-              a1("foo" => "123", "bazz" => "456")
+              a1 "hello"
+              a1 "goodbye"
             } }
           assert_node_hash_not_equal(*doc.css("a1"))
         end
       end
+    end
 
-      context "attributes reverse-engineered to be similar" do
+    context "elements with same name and children (with no attributes)" do
+      context "and children are in the same order" do
+        it "hashes equally" do
+          doc = xml { root {
+              a1 { b1 ; b2 }
+              a1 { b1 ; b2 }
+            } }
+          assert_node_hash_equal(*doc.css("a1"))
+        end
+      end
+
+      context "and children are not in the same order" do
         it "hashes differently" do
           doc = xml { root {
-              a1("foo" => "bar#{Diffaroo::Hash::SEP}quux")
-              a1("foo#{Diffaroo::Hash::SEP}bar" => "quux")
+              a1 { b1 ; b2 }
+              a1 { b2 ; b1 }
             } }
           assert_node_hash_not_equal(*doc.css("a1"))
         end
+      end
+    end
+
+    context "elements with same name and same attributes (with no content)" do
+      it "hashes equally" do
+        doc = xml { root {
+            a1("foo" => "bar", "bazz" => "quux")
+            a1("foo" => "bar", "bazz" => "quux")
+          } }
+        assert_node_hash_equal(*doc.css("a1"))
+      end
+    end
+
+    context "elements with same name and different attributes (with no content)" do
+      it "hashes differently" do
+        doc = xml { root {
+            a1("foo" => "bar", "bazz" => "quux")
+            a1("foo" => "123", "bazz" => "456")
+          } }
+        assert_node_hash_not_equal(*doc.css("a1"))
+      end
+    end
+
+    context "attributes reverse-engineered to be similar" do
+      it "hashes differently" do
+        doc = xml { root {
+            a1("foo" => "bar#{Diffaroo::Hash::SEP}quux")
+            a1("foo#{Diffaroo::Hash::SEP}bar" => "quux")
+          } }
+        assert_node_hash_not_equal(*doc.css("a1"))
       end
     end
 
     context "HTML" do
-      it "write some HTML tests"
-    end
-
-    it "hashes each node only once" do
-      doc = xml { root {
-          a1 {
-            b1 {
-              c1 "hello"
-            }
-          }
-        } }
-      node = doc.at_css "c1"
-      mock.proxy(Diffaroo::Hash).node_hash(anything).times(5)
-      Diffaroo::Hash.node_hash(doc.root)
+      it "write some case-insensitive HTML tests"
     end
   end
 end
