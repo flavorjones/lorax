261d5f878b65e74ea7ac7392cd563b85ff3d7314
Mike Dalessio <mike@csa.net>
2009-12-28 17:52:10 -0500

    moving signature_spec into spec/unit, because imma write some integration specs.

--- spec/unit/signature_spec.rb
+++ spec/unit/signature_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Diffaroo::Signature do
   def assert_node_signature_equal(node1, node2)
