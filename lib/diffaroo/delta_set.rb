db088200283d35f2f4fbc70c27d5b6bc365a53d3
Mike Dalessio <mike@csa.net>
2009-12-30 08:23:18 -0500

    cleaning up DeltaSet and its specs

--- lib/diffaroo/delta_set.rb
+++ lib/diffaroo/delta_set.rb
@@ -3,10 +3,12 @@ module Diffaroo
     attr_accessor :match_set, :deltas
 
     def initialize(match_set)
-      raise ArgumentError, "DeltaSet.new expects a MatchSet but received a #{match_set.class}" unless match_set.is_a?(MatchSet)
       @match_set = match_set
       @deltas = []
-      generate_deltas
+      if match_set
+        raise ArgumentError, "DeltaSet.new expects a MatchSet but received a #{match_set.class}" unless match_set.is_a?(MatchSet)
+        generate_deltas
+      end
     end
 
     def add(delta)
@@ -24,21 +26,22 @@ module Diffaroo
       document
     end
 
-    private
-
     def generate_deltas
       generate_inserts_and_moves_recursively match_set.signature2.root
       # generate_deletes_recursively
     end
 
+    private
+
     def generate_inserts_and_moves_recursively node
       match = match_set.match node
       if match.nil?
         add InsertDelta.new(node, node.parent.path)
+      else
+        node.children.each { |child| generate_inserts_and_moves_recursively child }
       end
       # return if match.perfect?
       # create an update delta
-      node.children.each { |child| generate_inserts_and_moves_recursively child }
     end
 
     # def generate_deletes_recursively
--- spec/unit/delta_set_spec.rb
+++ spec/unit/delta_set_spec.rb
@@ -2,35 +2,52 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Diffaroo::DeltaSet do
   describe ".new" do
-    it "takes a match set" do
+    context "is passed a MatchSet" do
+      before { @match_set = Diffaroo::MatchSet.new(xml { root }, xml { root }) }
+
+      it "does not raise an error" do
+        proc { Diffaroo::DeltaSet.new @match_set }.should_not raise_error
+      end
+
+      it "calls generate_deltas" do
+        mock.proxy.instance_of(Diffaroo::DeltaSet).generate_deltas
+        Diffaroo::DeltaSet.new @match_set
+      end
+    end
+
+    context "is passed a nil (hopefully for testing purposes" do
+      it "does not raise an error" do
+        proc { Diffaroo::DeltaSet.new nil }.should_not raise_error
+      end
+
+      it "does not call generate_deltas" do
+        mock.proxy.instance_of(Diffaroo::DeltaSet).generate_deltas.never
+        proc { Diffaroo::DeltaSet.new nil }.should_not raise_error
+      end
+    end
+
+    context "is passed non-nil non-MatchSet" do
+      it "raises an error" do
+        proc { Diffaroo::DeltaSet.new 1 }.should raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe "#match_set" do
+    it "returns the match set passed to #new" do
       match_set = Diffaroo::MatchSet.new(xml { root }, xml { root })
-      proc { Diffaroo::DeltaSet.new match_set }.should_not raise_error
-    end
-
-    it "raises an error if passed something that is not a match set" do
-      proc { Diffaroo::DeltaSet.new 1 }.should raise_error(ArgumentError)
-    end
-
-    it "generates insert delta" do
-      doc1 = xml { root }
-      doc2 = doc1.dup
-      match_set = Diffaroo::MatchSet.new doc1, doc2
       delta_set = Diffaroo::DeltaSet.new match_set
-
-      delta_set.deltas.size.should == 1
-      delta = delta_set.deltas.first
-      delta.should be_instance_of(Diffaroo::InsertDelta)
-      delta.xpath.should == "/"
-      delta.node.should == doc2.at_css("root")
+      delta_set.match_set.should == match_set
     end
   end
 
-  describe "#add" do
-    it "needs a spec"
-  end
-
-  describe "#deltas" do
-    it "needs a spec"
+  describe "#add / #deltas" do
+    it "appends to and returns an ordered list of deltas" do
+      delta_set = Diffaroo::DeltaSet.new nil
+      delta_set.add :foo
+      delta_set.add :bar
+      delta_set.deltas.should == [:foo, :bar]
+    end
   end
 
   describe "#apply" do
@@ -45,16 +62,24 @@ describe Diffaroo::DeltaSet do
   end
 
   describe "#apply!" do
-    context "when there are insert deltas" do
-      it "does something"
+    it "invokes apply! on each delta in order" do
+      doc = xml { root }
+      delta_set = Diffaroo::DeltaSet.new nil
+      delta1 = Diffaroo::InsertDelta.new(:foo, :bar)
+      delta2 = Diffaroo::InsertDelta.new(:foo, :bar)
+      delta_set.add delta1
+      delta_set.add delta2
+
+      order_of_invocation = []
+      mock(delta1).apply!(doc) { order_of_invocation << :delta1 }
+      mock(delta2).apply!(doc) { order_of_invocation << :delta2 }
+      delta_set.apply!(doc)
+
+      order_of_invocation.should == [:delta1, :delta2]
     end
   end
 
-  describe "#match_set" do
-    it "returns the match set passed to #new" do
-      match_set = Diffaroo::MatchSet.new(xml { root }, xml { root })
-      delta_set = Diffaroo::DeltaSet.new match_set
-      delta_set.match_set.should == match_set
-    end
+  describe "#generate_deltas" do
+    it "needs a spec"
   end
 end
