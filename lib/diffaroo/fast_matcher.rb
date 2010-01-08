41391a582afaadc1a1a1815e11f2c27fd38142f5
Mike Dalessio <mike@csa.net>
2010-01-08 01:34:37 -0500

    refactoring.

--- lib/diffaroo/fast_matcher.rb
+++ lib/diffaroo/fast_matcher.rb
@@ -2,33 +2,36 @@ module Diffaroo
   module FastMatcher
     def self.match(doc1, doc2)
       match_set = MatchSet.new(doc1, doc2)
-      match_recursively match_set, match_set.signature1.root
+      match_node match_set, match_set.signature1.root
       match_set
     end
 
     private
 
-    def self.match_recursively(match_set, node1)
+    def self.match_node(match_set, node1)
       return if match_set.match(node1)
-      sig1       = match_set.signature1.signature(node1) # assumes node1 is in signature1
-      candidates = match_set.signature2.nodes(sig1) || []
+      signature1 = match_set.signature1.signature(node1) # assumes node1 is in signature1
+      candidates = match_set.signature2.nodes(signature1) || []
       candidates.reject! { |node| match_set.match(node) }
 
       if candidates.empty?
         node1.children.each do |child|
-          match_recursively(match_set, child)
+          match_node(match_set, child)
         end
-        if match = match_set.match(node1)
-          downward_match(match.pair.first, match.pair.last, match_set)
-        end
+        match = match_set.match(node1)
+        downward_match(match_set, match.pair.first, match.pair.last) if match
       else
-        upward_matches = candidates.collect do |node2|
-          upward_match(node1, node2, match_depth(node2, match_set.signature2))
-        end
-        longest_trail = upward_matches.max { |a, b| a.length <=> b.length }
-        longest_trail.each do |ancestral_match|
-          match_set.add ancestral_match
-        end
+        match_candidate(match_set, node1, candidates)
+      end
+    end
+
+    def self.match_candidate(match_set, node1, candidates)
+      upward_matches = candidates.collect do |node2|
+        upward_match(node1, node2, depth(node2, match_set.signature2))
+      end
+      longest_trail = upward_matches.max { |a, b| a.length <=> b.length }
+      longest_trail.each do |ancestral_match|
+        match_set.add ancestral_match
       end
     end
 
@@ -43,7 +46,7 @@ module Diffaroo
       matches
     end
 
-    def self.downward_match(node1, node2, match_set)
+    def self.downward_match(match_set, node1, node2)
       # TODO: OMG! MY EYES ARE BLEEDING! REFACTOR ME AND OPTIMIZE ME!
       children_set1 = collect_children_by_name(node1.children, match_set)
       children_set2 = collect_children_by_name(node2.children, match_set)
@@ -55,7 +58,7 @@ module Diffaroo
             children2.each do |child2|
               if node1.children.index(child1) == node2.children.index(child2)
                 match_set.add Match.new(child1, child2)
-                downward_match(child1, child2, match_set)
+                downward_match(match_set, child1, child2)
               end
             end
           end
@@ -63,7 +66,7 @@ module Diffaroo
       end
     end
 
-    def self.match_depth(node, sig)
+    def self.depth(node, sig)
       depth = 1 + Math.log(sig.size) * sig.weight(node) / sig.weight
       # puts "diffaroo: debug: #{__FILE__}:#{__LINE__}: depth #{depth} = 1 + #{Math.log(sig.size)} * #{sig.weight(node)} / #{sig.weight}"
       depth.to_i
